40545c1a272c7884cc7fd109caf0302aa90696b2
Frits Talbot <frits@metapathy.com>
2017-09-11 22:50:52 +0200

    Change to RGgB scheme (g=I)

--- dpi/dpi.h
+++ dpi/dpi.h
@@ -8,10 +8,10 @@ namespace framebuffer
 	// 33222222222211111111110000000000
 	// 10987654321098765432109876543210
 	// AAAAAAAARRRRRRRRGGGGGGGGBBBBBBBB
-	static const int red_bit=2;
-	static const int green_bit=3;
-	static const int blue_bit=4;
-	static const int intensity_bit=5;
+	static const int red_bit=23;
+	static const int green_bit=15;
+	static const int blue_bit=7;
+	static const int intensity_bit=14;
 
 	static const char *fb_path="/dev/fb0";
 	static const char *tty_path="/dev/tty0";
